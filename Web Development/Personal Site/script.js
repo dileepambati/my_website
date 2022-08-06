ede685937321a191f089dcf85dc0b73efad2c81d
dileepambati <110690661+dileepambati@users.noreply.github.com>
2022-08-06 07:53:09 +0530

    Delete Web Development/Personal Site directory

--- Web Development/Personal Site/script.js
+++
@@ -1,9 +0,0 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toLocaleString();
-  document.getElementById("currentTime").style.color = "red";
-}
-showTime();
-setInterval(function () {
-
-	showTime();
-}, 1000);
