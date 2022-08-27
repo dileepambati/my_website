09497cbbd6a4b9b43f732a598b437148cc8e9588
dileepambati <110690661+dileepambati@users.noreply.github.com>
2022-08-27 15:30:36 +0530

    Delete script.js
    Signed-off-by: dileepambati <110690661+dileepambati@users.noreply.github.com>

--- script.js
+++
@@ -1,9 +0,0 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toLocaleString();
-  document.getElementById("currentTime").style.color = "black";
-}
-showTime();
-setInterval(function () {
-
-	showTime();
-}, 1000);
